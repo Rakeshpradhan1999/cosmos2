f16e99be66708d7c4be01b45a30ffaaae546c2f6
Rakesh Pradhan <pradhanrakesh558@gmail.com>
2021-09-21 17:24:46 +0530

    Update App.js
    ChainID updated to mainnet

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
     // you are connected to main net
     // Please connect to main net
 
-    if (chainId === 4) {
+    if (chainId === 1) {
       toast(`You are connected to main net`, {
         type: "success",
         position: toast.POSITION.BOTTOM_CENTER,
@@ -120,7 +120,7 @@ function App() {
 
   async function mint(mintCount) {
     if (contract) {
-      if (chainId === 4) {
+      if (chainId === 1) {
         if (mintCount === 0) {
           setLessMintAmountAlert(true);
         } else {
